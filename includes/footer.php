393d98fcb4f0051ee8be479fb9026075d219af34
5nax <97379931+5nax@users.noreply.github.com>
2023-08-11 12:26:54 +0545

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2023 Brought to You By Rajan Aryal
+</footer>
